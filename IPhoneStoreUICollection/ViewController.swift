cda4bcb19f107ba3f3e7b9b35704d1958f059176
kateyatsk <klaettoe2023@gmail.com>
2025-03-26 11:37:28 +0300

    Initial Commit

---
+++ IPhoneStoreUICollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPhoneStoreUICollection
+//
+//  Created by Екатерина Яцкевич on 26.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
